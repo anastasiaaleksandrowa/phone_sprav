4befa6d76bd8433da09f0132daab7480021fdba9
Kira <kirnan_t@outlok.com>
2024-06-26 21:57:13 +0700

    def search name

--- phone_sprav.py
+++ phone_sprav.py
@@ -13,3 +13,13 @@ def delete_contact(contact_id):
         del phone_book[contact_id]
     else:
          print("контакт не найден")
+
+# функция поиска контакта по имени 
+def search_contact(search_key):
+    for contact_id, contact_info in phone_book.items():
+        if search_key.lower() in contact_info ['last_name'].lower() or search_key.lower() in contact_info ['first_name'].lower():
+            print (f"ID: {contact_id}")
+            print (f"Фамилия: {contact_info['last_name']}")
+            print (f"имя: {contact_info['first_name']}")
+            print (f"отчество: {contact_info['middle_name']}")
+            print (f"номер телефона: {contact_info['phone_number']}")
